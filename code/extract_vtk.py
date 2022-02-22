c1fdd84d349ce38bf5029e7aa09cbd07251ed8e2
SaumikDana <saumik@utexas.edu>
2022-02-22 15:10:07 -0500

    latest

--- code/extract_vtk.py
+++ code/extract_vtk.py
@@ -1,28 +1,74 @@
+
 import numpy as np
-import matplotlib.pyplot as plt
 import vtk
-from vtk.util import numpy_support
 
-filename = "./vtk_plots/test_t0010.vtk"
-reader = vtk.vtkUnstructuredGridReader()
-reader.SetFileName(filename)
-reader.ReadAllVectorsOn()
-reader.ReadAllScalarsOn()
-reader.Update()
+class parse_vtk:
+   
+   def parse(self, filename):
+	"""
+	Method to parse the file `filename`. It returns a matrix with all the coordinates.
 
-cell2point = vtk.vtkCellDataToPointData()
-cell2point.SetInputData(reader.GetOutput())
-cell2point.Update()
+	:param string filename: name of the input file.
 
-#u = numpy_support.vtk_to_numpy(reader.GetOutput().GetCellData().GetArray('displacement'))
+	:return: mesh_points: it is a `n_points`-by-3 matrix containing the coordinates of
+		the points of the mesh
+	:rtype: numpy.ndarray
 
-coord = numpy_support.vtk_to_numpy(cell2point.GetOutput().GetPoints().GetData())
-x = coord[:,0]
-y = coord[:,1]
-z = coord[:,2]
+	.. todo::
 
-#temperature = numpy_support.vtk_to_numpy(cell2point.GetOutput().GetPointData().GetAbstractArray(1))
-#plt.tricontourf(x,y,temperature,15,cmap="jet")
-#plt.gca().set_aspect("equal")
-#plt.colorbar()
-#plt.show()
+		- specify when it works
+	"""
+	self.infile = filename
+
+	reader = vtk.vtkDataSetReader()
+	reader.SetFileName(self.infile)
+	reader.ReadAllVectorsOn()
+	reader.ReadAllScalarsOn()
+	reader.Update()
+	data = reader.GetOutput()
+
+	n_points = data.GetNumberOfPoints()
+	mesh_points = np.zeros([n_points, 3])
+
+	for i in range(n_points):
+		mesh_points[i][0], mesh_points[i][1], mesh_points[i][
+			2
+		] = data.GetPoint(i)
+
+	return mesh_points
+
+if __name__ == '__main__':
+
+   filename = "./vtk_plots/test_t0010.vtk"
+   parser = parse_vtk()
+   coords = parser.parse(filename)
+
+#import numpy as np
+#import vtk
+#from vtk.util.numpy_support import vtk_to_numpy
+#
+#filename = "./vtk_plots/test_t0010.vtk"
+#reader = vtk.vtkDataSetReader()
+#reader.SetFileName(filename)
+#reader.ReadAllVectorsOn()
+#reader.ReadAllScalarsOn()
+#reader.Update()
+#data = reader.GetOutput()
+#
+#print('--- Mesh information ---')
+#
+#print('Number of cells is %s, Number of pieces is %s, Number of points is %s' % (data.GetNumberOfCells(), data.GetNumberOfPieces(), data.GetNumberOfPoints()))  
+#
+#print('--- Mesh information ---')
+#
+#d=data.GetPointData()
+#
+#array=d.GetArray('displacement')
+#
+#print(type(array))
+#
+#print('Number of scalars in file is %s' % reader.GetNumberOfScalarsInFile())
+#
+#print('Number of vectors in file is %s' % reader.GetNumberOfVectorsInFile())
+#
+#print(reader.GetVectorsNameInFile(0))
