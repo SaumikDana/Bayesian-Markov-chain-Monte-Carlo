819f1d3369b1424791a2cd1d8307944a0d7454da
SaumikDana <saumik@utexas.edu>
2022-02-22 04:13:36 -0500

    latest

--- code/extract_vtk.py
+++ code/extract_vtk.py
@@ -3,23 +3,26 @@ import matplotlib.pyplot as plt
 import vtk
 from vtk.util import numpy_support
 
-filename = "heat_result.vtk"
+filename = "./vtk_plots/test_t0010.vtk"
 reader = vtk.vtkUnstructuredGridReader()
 reader.SetFileName(filename)
+reader.ReadAllVectorsOn()
+reader.ReadAllScalarsOn()
 reader.Update()
 
 cell2point = vtk.vtkCellDataToPointData()
 cell2point.SetInputData(reader.GetOutput())
 cell2point.Update()
 
+#u = numpy_support.vtk_to_numpy(reader.GetOutput().GetCellData().GetArray('displacement'))
+
 coord = numpy_support.vtk_to_numpy(cell2point.GetOutput().GetPoints().GetData())
 x = coord[:,0]
 y = coord[:,1]
 z = coord[:,2]
-temperature = numpy_support.vtk_to_numpy(cell2point.GetOutput().GetPointData().GetAbstractArray(1))
 
-plt.tricontourf(x,y,temperature,15,cmap="jet")
-plt.gca().set_aspect("equal")
-#plt.axis('equal')
-plt.colorbar()
-plt.show()
+#temperature = numpy_support.vtk_to_numpy(cell2point.GetOutput().GetPointData().GetAbstractArray(1))
+#plt.tricontourf(x,y,temperature,15,cmap="jet")
+#plt.gca().set_aspect("equal")
+#plt.colorbar()
+#plt.show()
