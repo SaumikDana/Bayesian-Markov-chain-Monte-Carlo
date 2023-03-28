4fc1b9873c1bbf356a4c409e029e7fc9d370d86c
Saumik Dana <saumik@utexas.edu>
2023-03-27 23:47:59 -0500

    Update dl_inference.py

--- code/dl_inference.py
+++ code/dl_inference.py
@@ -1,5 +1,4 @@
 from rsf import rsf
-from pylith_gprs import pylith_gprs
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
@@ -7,9 +6,8 @@ import argparse
 
 if __name__ == '__main__':
 
-    #Usage: python dl_inference.py -problem coupled -epochs 100 -samples 100 --bayesian
+    #Usage: python dl_inference.py -epochs 100 -samples 100 --bayesian
     parser = argparse.ArgumentParser()
-    parser.add_argument('-problem', dest='problem', type=str, help="Problem type")
     parser.add_argument('-epochs',dest='num_epochs', type=int, help="Number of Epochs")
     parser.add_argument('-samples',dest='num_samples', type=int, help="Number of Samples")
     parser.add_argument('--reduction', dest='reduction', action='store_true')
@@ -19,12 +17,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # rsf problem
-    if args.problem == 'rsf':
-       problem = rsf(args)
-    # pylith-gprs problem
-    elif args.problem == 'coupled':
-       problem = pylith_gprs(args)
-
+    problem = rsf(args)
+    
     # Solve the problem
     problem.solve()
 
