4a283fc4d265032e8abf6d309d75de34c5738fa5
Saumik Dana <saumik@sapientai.io>
2023-04-02 20:38:29 -0500

    optimize

--- code/MCMC.py
+++ code/MCMC.py
@@ -14,57 +14,53 @@ class MCMC:
         lstm_model={}, adapt_interval=100, 
         verbose=True):
 
-        self.model             = model
-        self.qstart            = qstart
-        self.qpriors           = qpriors
-        self.nsamples          = nsamples # number of samples to take during MCMC algorithm
-        self.nburn             = int(nsamples/2) # number of samples to discard during burn-in period
-        self.verbose           = verbose
-        self.adapt_interval    = adapt_interval
-        self.data              = data
-        self.lstm_model        = lstm_model
-        self.problem_type      = problem_type
-        self.n0                = 0.01
+        self.model = model
+        self.qstart = qstart
+        self.qpriors = qpriors
+        self.nsamples = nsamples # number of samples to take during MCMC algorithm
+        self.nburn = int(nsamples/2) # number of samples to discard during burn-in period
+        self.verbose = verbose
+        self.adapt_interval = adapt_interval
+        self.data = data
+        self.lstm_model = lstm_model
+        self.problem_type = problem_type
+        self.n0 = 0.01
 
-        # Construct the initial covariance matrix
-        acc_                   = self.model.evaluate(problem_type,lstm_model)[1]
-        self.model.Dc         *= (1+1e-6) # perturb the dc
-        acc_dq_                = self.model.evaluate()[1]
-        acc                    = acc_[:, 0].reshape(1, -1)
-        self.std2              = [np.sum((acc - self.data) ** 2, axis=1).item()/(acc.shape[1] - len(self.qpriors))]
-        acc_dq                 = acc_dq_[:, 0].reshape(1, -1)
-        X                      = ((acc_dq - acc) / (self.model.Dc * 1e-6)).T
-        X                      = np.linalg.inv(np.dot(X.T, X))
-        self.Vstart            = self.std2[-1] * X  
+        # Evaluate the model with the original dc value
+        acc_ = self.model.evaluate(problem_type, lstm_model)[1]
+        # Perturb the dc value
+        self.model.Dc *= (1 + 1e-6)
+        # Evaluate the model with the perturbed dc value
+        acc_dq_ = self.model.evaluate()[1]
+        # Extract the accuracy values and reshape them to a 1D array
+        acc = acc_[:, 0].reshape(1, -1)
+        # Compute the variance of the noise
+        self.std2 = [np.sum((acc - self.data) ** 2, axis=1).item() / (acc.shape[1] - len(self.qpriors))]
+        # Extract the accuracy gradient values and reshape them to a 1D array
+        acc_dq = acc_dq_[:, 0].reshape(1, -1)
+        # Compute the covariance matrix
+        X = ((acc_dq - acc) / (self.model.Dc * 1e-6)).T
+        X = np.linalg.inv(np.dot(X.T, X))
+        self.Vstart = self.std2[-1] * X 
 
-        # Set up the initial qstart vector and qstart limits
-        self.qstart_vect       = np.zeros((1, 1))
-        self.qstart_limits     = np.zeros((1, 2))
-        self.qstart_vect[0, 0] = self.qstart
-        self.qstart_limits[0, 0] = self.qpriors[1]
-        self.qstart_limits[0, 1] = self.qpriors[2]
-            
         return
 
     def sample(self):
-        """
-        Function for sampling using adaptive Metropolis algorithm
-
-        Return:
-            Q_MCMC: Accepted samples
-        """
 
         # Initialize variables
-        qparams           = copy.deepcopy(self.qstart_vect) # Array of sampled parameters
-        Vold              = copy.deepcopy(self.Vstart) # Covariance matrix of previously sampled parameters
-        Vnew              = copy.deepcopy(self.Vstart) # Covariance matrix of current sampled parameters
-        SSqprev           = self.SSqcalc(qparams) # Squared error of previously sampled parameters
-        iaccept           = 0 # Counter for accepted samples
+        qstart_vect = np.array([[self.qstart]])
+        self.qstart_limits = np.array([[self.qpriors[1], self.qpriors[2]]])
+        qparams = np.copy(qstart_vect) # Array of sampled parameters
+        Vold = np.copy(self.Vstart) # Covariance matrix of previously sampled parameters
+        Vnew = np.copy(self.Vstart) # Covariance matrix of current sampled parameters
+
+        SSqprev = self.SSqcalc(qparams) # Squared error of previously sampled parameters
+        iaccept = 0 # Counter for accepted samples
 
         # Loop over number of desired samples
-        for isample in range(self.nsamples):
+        for isample in np.arange(self.nsamples):
             # Sample new parameters from a normal distribution with mean being the last element of qparams
-            q_new         = np.reshape(np.random.multivariate_normal(qparams[:,-1],Vold),(-1,1)) 
+            q_new = np.reshape(np.random.multivariate_normal(qparams[:,-1],Vold),(-1,1)) 
             # Accept or reject the new sample based on the Metropolis-Hastings acceptance rule
             accept,SSqnew = self.acceptreject(q_new,SSqprev,self.std2[-1])
             # Print some diagnostic information
@@ -72,27 +68,27 @@ class MCMC:
             print("Generated sample ---- ",np.asscalar(q_new))
             # If the new sample is accepted, add it to the list of sampled parameters
             if accept:
-                qparams   = np.concatenate((qparams,q_new),axis=1)
-                SSqprev   = copy.deepcopy(SSqnew)
-                iaccept  += 1
+                qparams = np.concatenate((qparams,q_new),axis=1)
+                SSqprev = SSqnew.copy()
+                iaccept += 1
             else:
                 # If the new sample is rejected, add the previous sample to the list of sampled parameters
-                q_new     = np.reshape(qparams[:,-1],(-1,1))
-                qparams   = np.concatenate((qparams,q_new),axis=1)
+                q_new = np.reshape(qparams[:,-1],(-1,1))
+                qparams = np.concatenate((qparams,q_new),axis=1)
 
             # Update the estimate of the standard deviation
-            aval          = 0.5*(self.n0+self.data.shape[1])
-            bval          = 0.5*(self.n0*self.std2[-1]+SSqprev)
+            aval = 0.5*(self.n0+self.data.shape[1])
+            bval = 0.5*(self.n0*self.std2[-1]+SSqprev)
             self.std2.append(1/gamma.rvs(aval,scale=1/bval,size=1)[0])
 
             # Update the covariance matrix if it is time to adapt it
             if (isample+1) % self.adapt_interval == 0:
                 try:
-                    Vnew     = 2.38**2/len(self.qpriors.keys())*np.cov(qparams[:,-self.adapt_interval:])
+                    Vnew = 2.38**2/len(self.qpriors.keys())*np.cov(qparams[:,-self.adapt_interval:])
                     if qparams.shape[0]==1:
                         Vnew = np.reshape(Vnew,(-1,1))
                     R = np.linalg.cholesky(Vnew)
-                    Vold = copy.deepcopy(Vnew)
+                    Vold = Vnew.copy()
                 except:
                     pass
 
@@ -100,32 +96,20 @@ class MCMC:
         print("acceptance ratio:",iaccept/self.nsamples)
 
         # Return accepted samples
-        self.std2 = np.asarray(self.std2)
-        self.std2 = self.std2[self.nburn:] 
+        self.std2 = np.asarray(self.std2)[self.nburn:] 
         # Trim the estimate of the standard deviation to exclude burn-in samples
         return qparams[:,self.nburn:]
 
     def acceptreject(self, q_new, SSqprev, std2):
-        """
-        Implementation of the accept-reject step in Metropolis-Hastings algorithm.
 
-        Parameters:
-            q_new (numpy array): A numpy array representing the new proposal values for the parameters.
-            SSqprev (float):     A float representing the sum of squares error of the previous proposal.
-            std2 (float):        A float representing the variance of the distribution used to generate the proposal.
-
-        Returns:
-            tuple: A tuple containing a boolean indicating whether the proposal is accepted or rejected, and 
-                   the sum of squares error of the proposal (either the previous or the new one).
-        """
         # Check if the proposal values are within the limits
-        accept = np.all(q_new[:, 0] > self.qstart_limits[:, 0]) and np.all(q_new[:, 0] < self.qstart_limits[:, 1])
+        accept = np.all((q_new > self.qstart_limits[:, 0]) & (q_new < self.qstart_limits[:, 1]), axis=0)
 
         if accept:
             # Compute the sum of squares error of the new proposal
             SSqnew = self.SSqcalc(q_new)
             # Compute the acceptance probability
-            accept_prob = min(0, 0.5*(SSqprev - SSqnew) / std2)
+            accept_prob = np.clip(0.5 * (SSqprev - SSqnew) / std2, -np.inf, 0)
             # Check if the proposal is accepted based on the acceptance probability and a random number
             accept = accept_prob > np.log(np.random.rand(1))[0]
 
@@ -137,19 +121,13 @@ class MCMC:
             return accept, SSqprev
 
     def SSqcalc(self, q_new):
-        """
-        Compute the sum of squares error of the proposed parameter values.
 
-        Parameters:
-            q_new (numpy array): A numpy array representing the proposed values for the parameters.
-
-        Returns:
-            numpy array: A numpy array representing the sum of squares error of the proposed parameter values.
-        """
+        # Update the Dc parameter of the model with the new proposal
         self.model.Dc = q_new[0,]
-        acc_          = self.model.evaluate(self.problem_type,self.lstm_model)[1] 
-        acc           = acc_[:, 0].reshape(1, -1)
-        SSq           = np.sum((acc - self.data)**2, axis=1) # squared error
+        # Evaluate the model's performance on the problem type and LSTM model
+        acc = self.model.evaluate(self.problem_type, self.lstm_model)[1]
+        # Compute the sum of squares error between the model's accuracy and the data
+        SSq = np.sum((acc[:, 0].reshape(1, -1) - self.data)**2, axis=1, keepdims=True)
 
         return SSq
 
