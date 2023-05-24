f2a1854ab5615d48ec1957960d747c2fe300600b
Saumik Dana <saumik@sapientai.io>
2023-05-24 02:12:31 -0500

    edits

--- code/dl_inference.cpp
+++ code/dl_inference.cpp
@@ -7,15 +7,15 @@ using namespace std;
 
 class RateStateModel {
 public:
-    int num_tsteps;
-    double Dc;
-    double t_start;
-    double t_final;
+    int num_tsteps;     // Number of time steps
+    double Dc;          // Critical slip distance
+    double t_start;     // Start time
+    double t_final;     // Final time
 
     double random_normal() {
-        static std::mt19937 generator;
-        static std::normal_distribution<double> distribution(0.0, 1.0);
-        return distribution(generator);
+        static std::mt19937 generator;                                      // Random number generator
+        static std::normal_distribution<double> distribution(0.0, 1.0);     // Normal distribution with mean 0 and standard deviation 1
+        return distribution(generator);                                     // Return a random number from the distribution
     }
 
     RateStateModel() {
@@ -26,8 +26,9 @@ public:
     }
 
     vector<vector<double> > evaluate() {
-        vector<vector<double> > result(num_tsteps, vector<double>(2));
+        vector<vector<double> > result(num_tsteps, vector<double>(2));      // Vector to store the results
 
+        // Constants and parameters
         double a = 0.011;
         double b = 0.014;
         double mu_ref = 0.6;
@@ -41,6 +42,7 @@ public:
         bool RadiationDamping = true;
         double Dc = this->Dc;
 
+        // Initialize variables
         vector<double> t(num_tsteps);
         vector<double> mu(num_tsteps);
         vector<double> theta(num_tsteps);
@@ -55,6 +57,7 @@ public:
 
         int num_steps = static_cast<int>(floor((t_final - t_start) / delta_t));
 
+        // Friction function
         auto friction = [&](double t, vector<double> y) {
             double V_ref = V_ref;
             double a = a;
@@ -81,6 +84,7 @@ public:
             return dydt;
         };
 
+        // Initialize state variables
         vector<double> y(3, 0.0);
         y[0] = mu_t_zero;
         y[1] = theta[0];
@@ -88,11 +92,15 @@ public:
 
         int k = 1;
         while (k < num_steps) {
+            // Calculate derivatives using friction function
             vector<double> yprime = friction(t[k - 1], y);
+
+            // Update state variables using Euler's method
             for (int i = 0; i < 3; i++) {
                 y[i] += delta_t * yprime[i];
             }
 
+            // Update time and other variables
             t[k] = t[k - 1] + delta_t;
             mu[k] = y[0];
             theta[k] = y[1];
@@ -102,11 +110,13 @@ public:
             k++;
         }
 
+        // Add noise to acceleration
         vector<double> acc_noise(acc.size(), 0.0);
         for (size_t i = 0; i < acc.size(); i++) {
             acc_noise[i] = acc[i] + 1.0 * abs(acc[i]) * random_normal();
         }
 
+        // Prepare results for output
         vector<vector<double> > t_acc_noise(num_steps, vector<double>(2));
         vector<vector<double> > acc_vec(num_steps, vector<double>(2));
 
@@ -119,23 +129,21 @@ public:
 
         return t_acc_noise;
     }
-
-
 };
 
 class rsf {
 public:
-    int num_dc;
-    double lowest_slip_value;
-    double largest_slip_value;
-    vector<double> dc_list;
-    string lstm_file;
-    string data_file;
-    int num_features;
-    bool plotfigs;
-    RateStateModel model;
-    vector<vector<double> > t_appended;
-    vector<vector<double> > acc_appended;
+    int num_dc;                 // Number of slip values
+    double lowest_slip_value;   // Lowest slip value
+    double largest_slip_value;  // Largest slip value
+    vector<double> dc_list;     // List of critical slip distances
+    string lstm_file;           // LSTM model file
+    string data_file;           // Data file
+    int num_features;           // Number of features
+    bool plotfigs;              // Flag for plotting figures
+    RateStateModel model;       // Rate State Model
+    vector<vector<double> > t_appended;     // Appended time data
+    vector<vector<double> > acc_appended;   // Appended acceleration data
 
     rsf(int number_slip_values = 1, double lowest_slip_value = 1.0, double largest_slip_value = 1000.0, bool plotfigs = false)
         : num_dc(number_slip_values), lowest_slip_value(lowest_slip_value), largest_slip_value(largest_slip_value), plotfigs(plotfigs) {
@@ -179,7 +187,6 @@ public:
             count_dc++;
         }
     }
-
 };
 
 int main() {
