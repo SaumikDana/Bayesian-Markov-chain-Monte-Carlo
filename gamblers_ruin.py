84d4f547d5d426d3fdf8c1faf3e384f6e08120eb
Saumik Dana <saumik@sapientai.io>
2023-05-27 03:07:00 -0500

    common brainteasers

--- gamblers_ruin.py
+++ gamblers_ruin.py
@@ -1,33 +1,37 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import random
 
-def solve_equation(N, p):
-    x = [0] * (N + 1)
-    x[0] = 1
-    x[N] = N
+def gamblers_ruin(initial_money, goal, win_probability):
+    """
+    Simulates the Gambler's Ruin problem and returns the probability of losing all money before reaching the goal.
+    Args:
+        initial_money (int): The initial amount of money the gambler has.
+        goal (int): The target amount of money the gambler wants to reach.
+        win_probability (float): The probability of winning each round.
+    Returns:
+        float: The probability of losing all money before reaching the goal.
+    """
+    num_simulations = 100000  # Number of simulations to run
+    num_loss = 0  # Counter for the number of times the gambler loses
+    
+    for _ in range(num_simulations):
+        money = initial_money
+        
+        while money > 0 and money < goal:
+            if random.random() < win_probability:
+                money += 1  # Win: increase money by 1
+            else:
+                money -= 1  # Loss: decrease money by 1
+        
+        if money == 0:
+            num_loss += 1
+    
+    probability_loss = num_loss / num_simulations
+    return probability_loss
 
-    for i in range(1, N):
-        x[i+1] = (x[i] - (1-p)*x[i-1]) / p
+# Example usage
+initial_money = 10
+goal = 20
+win_probability = 0.5
 
-    return x[1:N]
-
-# Range of p values
-p_values = np.linspace(0.5, 1, num=100)
-
-# Empty lists to store results
-x0_values = []
-
-# Solve the equation for different p values
-N = 10  # Number of elements
-point = int(N/2)
-for p in p_values:
-    solution = solve_equation(N, p)
-    x0_values.append(solution[point])
-
-# Plotting
-plt.plot(p_values, x0_values)
-plt.xlabel('p')
-plt.ylabel(f'x[{point}]')
-plt.title(f'Plot of x[{point}] against different values of p')
-plt.grid(True)
-plt.show()
+probability_loss = gamblers_ruin(initial_money, goal, win_probability)
+print(f"The probability of losing all money before reaching the goal is: {probability_loss}")
