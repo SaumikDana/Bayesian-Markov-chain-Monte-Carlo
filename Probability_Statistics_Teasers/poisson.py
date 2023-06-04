3c126b51bce540a158a8af64821f5b91f1435fb9
Saumik Dana <saumik@sapientai.io>
2023-06-04 01:25:23 -0500

    plots

--- Probability_Statistics_Teasers/poisson.py
+++ Probability_Statistics_Teasers/poisson.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import special
 
 # Set the lambda parameter for the Poisson distribution
 lambda_param = 3
@@ -8,12 +9,24 @@ lambda_param = 3
 x = np.arange(0, 15)
 
 # Calculate the probability mass function (PMF) using the Poisson formula
-factorial = np.vectorize(np.math.factorial)  # Vectorize the factorial function
-pmf = np.exp(-lambda_param) * (lambda_param ** x.astype(int)) / factorial(x)
+pmf = np.exp(-lambda_param) * (lambda_param ** x.astype(int)) / special.factorial(x)
+
+# Calculate the cumulative probabilities iteratively
+cdf = np.cumsum(pmf)
 
 # Plot the PMF
+plt.subplot(2, 1, 1)
 plt.bar(x, pmf, alpha=0.5)
 plt.xlabel('Number of Events')
 plt.ylabel('Probability')
-plt.title('Poisson Distribution (λ=3)')
+plt.title('Poisson Distribution (λ=3) - PMF')
+
+# Plot the cumulative probabilities
+plt.subplot(2, 1, 2)
+plt.plot(x, cdf, '-o')
+plt.xlabel('Number of Events')
+plt.ylabel('Cumulative Probability')
+plt.title('Poisson Distribution (λ=3) - CDF (Explicit Integration)')
+
+plt.tight_layout()
 plt.show()
