aed310f0072fd54049d2e115f830b54834180fde
Saumik Dana <saumik.dana@advancedscanners.com>
2023-11-17 02:05:58 -0600

    FastAPI second imprint

--- Python/main.py
+++ Python/main.py
@@ -1,7 +1,25 @@
+# main.py
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from simulation import run_simulation, visualize_data
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class SimulationParams(BaseModel):
+    qpriors: List
+    format: str  # Including the format parameter
+    nsamples: int
+
+@app.post("/run-simulation")
+def run_simulation_endpoint(params: SimulationParams):
+    if params.format not in ["json", "mysql"]:
+        return {"error": "Invalid format specified"}
+
+    time_taken = run_simulation(params.qpriors, params.format, params.nsamples)
+    return {"time_taken": time_taken}
+
+@app.get("/visualize")
+def visualize_endpoint():
+    visualize_data()
+    return {"message": "Data visualization initiated"}
