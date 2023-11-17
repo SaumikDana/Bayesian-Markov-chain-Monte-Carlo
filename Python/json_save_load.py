5a69988e4855723510433757dcb12726ffe151ff
Saumik Dana <saumik.dana@advancedscanners.com>
2023-11-17 13:40:17 -0600

    Refactored

--- Python/json_save_load.py
+++ Python/json_save_load.py
@@ -2,26 +2,29 @@ import json
 import numpy as np
 
 def numpy_array_encoder(obj):
+    """Custom JSON encoder for NumPy arrays."""
     if isinstance(obj, np.ndarray):
-        return obj.tolist()  # Convert the NumPy array to a nested list
-    raise TypeError("Object of type '{}' is not JSON serializable".format(type(obj)))
+        return {"__ndarray__": True, "data": obj.tolist(), "shape": obj.shape}
+    raise TypeError(f"Object of type '{type(obj).__name__}' is not JSON serializable")
 
 def save_object(obj, filename):
+    """Saves a Python object, including NumPy arrays, to a JSON file."""
     try:
         with open(filename, "w") as f:
             json.dump(obj, f, default=numpy_array_encoder)
     except Exception as ex:
-        print("Error during JSON serialization:", ex)
+        print(f"Error during JSON serialization: {ex}")
 
 def numpy_array_decoder(dct):
-    if "__ndarray__" in dct:
-        arr_data = np.asarray(dct["data"])
-        return arr_data.reshape(dct["shape"])
+    """Custom JSON decoder for NumPy arrays."""
+    if dct.get("__ndarray__"):
+        return np.array(dct["data"]).reshape(dct["shape"])
     return dct
 
 def load_object(filename):
+    """Loads a Python object, including NumPy arrays, from a JSON file."""
     try:
         with open(filename, "r") as f:
             return json.load(f, object_hook=numpy_array_decoder)
     except Exception as ex:
-        print("Error during JSON deserialization:", ex)
+        print(f"Error during JSON deserialization: {ex}")
