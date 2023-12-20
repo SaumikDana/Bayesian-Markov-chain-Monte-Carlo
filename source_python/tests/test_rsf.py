d65cd9285e3f1ecfa164e1d04580bde4bd7f9d83
Saumik Dana <dana.spk5@gmail.com>
2023-12-20 02:58:12 -0600

    updates

--- source_python/tests/test_rsf.py
+++ source_python/tests/test_rsf.py
@@ -1,60 +1,38 @@
 import pytest
 import numpy as np
-from ..RSF import RSF, measure_execution_time
+from ..RSF import RSF  # Adjust the import path as needed
 
 class TestRSF:
     @pytest.fixture
     def mock_model(self):
-        # Create a mock model object with necessary attributes and methods
-        class MockModel:
-            num_tsteps = 10
-            t_start = 0
-            t_final = 100
-            Dc = 1
+        # Mock RateStateModel class
+        class MockRateStateModel:
+            num_tsteps = 500
+            t_start = 0.0
+            t_final = 50.0
+            Dc = 500.0
 
             def evaluate(self):
-                # Return mock evaluation results
-                time_series = np.linspace(self.t_start, self.t_final, self.num_tsteps)
-                acceleration = np.random.randn(self.num_tsteps)
-                acc_noise = acceleration + np.random.normal(0, 0.1, self.num_tsteps)
-                return time_series, acceleration, acc_noise
+                # Mock implementation
+                return np.linspace(self.t_start, self.t_final, self.num_tsteps), np.random.rand(self.num_tsteps), np.random.rand(self.num_tsteps)
 
-        return MockModel()
+        return MockRateStateModel()
 
     @pytest.fixture
     def rsf_instance(self, mock_model):
-        # Initialize RSF instance with mock model and other parameters
-        return RSF(
-            number_slip_values=5, 
-            lowest_slip_value=1.0, 
-            largest_slip_value=1000.0,
-            qstart=10.0,
+        rsf = RSF(
+            number_slip_values=5,
+            lowest_slip_value=100.0,
+            largest_slip_value=5000.0,
+            qstart=1000.0,
             qpriors=["Uniform", 0.0, 10000.0],
-            reduction=False, 
-            plotfigs=False,
-            model=mock_model
+            plotfigs=False
         )
+        rsf.model = mock_model
+        return rsf
 
     def test_generate_time_series(self, rsf_instance):
-        # Test generate_time_series method
+        # Example test for the generate_time_series method
         result = rsf_instance.generate_time_series()
-        assert isinstance(result, np.ndarray)
-        assert result.shape[0] == rsf_instance.model.num_tsteps * len(rsf_instance.dc_list)
-
-    def test_prepare_data(self, rsf_instance):
-        # Test prepare_data method
-        # You may need to mock external dependencies like file or database operations
-        data = np.random.randn(100)
-        result = rsf_instance.prepare_data(data)
-        assert result is not None  # Add appropriate assertions based on your method's logic
-
-    # Add more test methods for other functionalities of the RSF class
-
-# Test for the measure_execution_time decorator
-def test_measure_execution_time():
-    @measure_execution_time
-    def dummy_function():
-        return "test"
-
-    execution_time = dummy_function()
-    assert isinstance(execution_time, float)
+        assert isinstance(result, np.ndarray), "generate_time_series should return a numpy array"
+        assert len(result) == rsf_instance.model.num_tsteps * rsf_instance.num_dc, "Length of result array should match expected size"
